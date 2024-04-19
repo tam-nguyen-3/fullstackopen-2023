aa9d0507e7245a650f8061e37e1e424d150f49ff
Tam Nguyen <tamngminh3@gmail.com>
2024-04-18 19:23:09 -0700

    update

--- part2/example/src/App.js
+++ part2/example/src/App.js
@@ -1,23 +1,20 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
+
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState()
   const [showAll, setShowAll] = useState(true)
  
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-  }
-  
-  useEffect(hook, [])
+  }, [])
   
   console.log('render', notes.length, 'notes')
 
@@ -28,9 +25,13 @@ const App = () => {
       important: Math.random() < 0.5,
       id: notes.length + 1,
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -42,6 +43,17 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -52,7 +64,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+          key={note.id} 
+          note={note} 
+          toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
+
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -29,9 +36,14 @@ const App = () => {
     }
 
     else {
-      setPersons(persons.concat(personObj))
-      setNewName('')
-      setNewNum('')
+      // setPersons(persons.concat(personObj))
+      // setNewName('')
+      // setNewNum('')
+      axios
+        .post('http://localhost:3001/persons', personObj)
+        .then(response => {
+          console.log(response)
+        })
     }
   }
 
