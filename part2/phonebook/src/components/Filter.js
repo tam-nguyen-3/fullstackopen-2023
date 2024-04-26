0382fe8e27c6245a1dafbb7ec6a1cd8eae8956de
Tam Nguyen <tamngminh3@gmail.com>
2024-04-26 15:04:35 +0700

    cleaning the tree

--- part2/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({newFilter, handleFilterChange}) => {
-    return (
-      <div>
-        filter shown with <input value={newFilter} onChange={handleFilterChange}></input>
-      </div>
-    )
-}
-
-export default Filter
--- part2/phonebook/src/components/Persons.js
+++
@@ -1,9 +0,0 @@
-const Person = ({person}) => <p> {person.name} {person.number} </p>
-
-const Persons = ({personList}) => {
-  return (
-    personList.map(person => <Person key={person.name} person={person}></Person>)
-  )
-}
-
-export default Persons
