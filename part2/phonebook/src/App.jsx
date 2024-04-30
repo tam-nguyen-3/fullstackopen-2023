8606a2ec745000f84b330c027c6075bd331b9e87
Tam Nguyen <tamngminh3@gmail.com>
2024-04-30 16:38:14 +0700

    part 2d

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import personService from './components/persons'
 
 const Input = ({text, value, handleChange}) => {
   return (
@@ -24,34 +26,43 @@ const PersonForm = ({formHandler, name, handleName, number, handleNumber}) => {
   )
 }
 
-const Detail = ({name, number}) => <p key={name}>{name} {number}</p>
-
-const Persons = ({persons}) => {
+const Detail = ({name, number, handleClick}) => {
   return (
-    persons.map((person) => <Detail name={person.name} number={person.number}></Detail>)
+    <div>
+      {/* <p key={name}>{name} {number}</p> <button onClick={handleClick}>delete</button> */}
+      {name} {number} <button onClick={handleClick}>delete</button>
+    </div>
+  )
+} 
+
+const Persons = ({persons, handleClick}) => {
+  return (
+    persons.map((person) => <Detail key={person.id} name={person.name} number={person.number} handleClick={() => handleClick(person.id)}></Detail>)
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => setPersons(initialPersons))
+  }, [])
+
   const personsToShow = (newFilter.length === 0) 
     ? persons
-    : persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()))
-    /* more understandable code:
     : persons.filter((person) => {
       const name = person.name.toLowerCase()
       const filter = newFilter.toLowerCase()
       return (name.includes(filter))
     })
+
+    /* one-liner:
+    : persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()))
     */
 
   const handleNameChange = (event) => {
@@ -70,19 +81,33 @@ const App = () => {
     event.preventDefault()
 
     if (persons.findIndex((person) => person.name === newName) != -1) {
+      
       return (alert(`${newName} is already added to phonebook`))
     }
 
-    setPersons(persons.concat({
-      id: persons.length + 1,
+    const newPerson = {
+      // id: persons.length + 1,
       name: newName,
       number: newNumber
-    }))
-    setNewName('')
-    setNewNumber('')
+    }
+
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
-  console.log(persons)
+  const handleDelete = (id) => {
+    const personToDelete = persons.find(person => person.id === id)
+    if (confirm(`Delete ${personToDelete.name}?`)) {
+      personService
+      .remove(id)
+      setPersons(persons.filter(p => p.id != id))
+    }
+  }
 
   return (
     <div>
@@ -91,7 +116,7 @@ const App = () => {
       <h2>add a new</h2>
       <PersonForm formHandler={addName} name={newName} handleName={handleNameChange} number={newNumber} handleNumber={handleNumberChange}></PersonForm>
       <h2>Numbers</h2>
-      <Persons persons={personsToShow}></Persons>
+      <Persons persons={personsToShow} handleClick={handleDelete}></Persons>
     </div>
   )
 }
